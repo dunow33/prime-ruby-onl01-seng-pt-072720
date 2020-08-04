ca2f56f4eb380df9ac4976947cfe95ca4f771743
Brian Donnoe <dunow33@hotmail.com>
2020-08-04 20:23:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(integer)
   if integer < 0
-    
-  
-  numbers = (2...integer).to_a
+    numbers = (integer...-2).to_a
+  else
+    numbers = (2...integer).to_a
+  end
   
   numbers.each do |num|
     if integer % num == 0
